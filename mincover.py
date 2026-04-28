27f8594f7ff0bbf21a71028471bfd828e8828615
IvanGorbache <Ivan.Gorbache@msmail.ariel.ac.il>
2026-04-28 16:17:53 +0300

    upload

--- mincover.py
+++ mincover.py
@@ -3,9 +3,11 @@
 # subprocess.check_call([sys.executable, "-m", "pip", "install", "networkx>=3.4"], stdout=subprocess.DEVNULL)
 
 import networkx as nx, cvxpy, numpy as np
+
 np.float_ = np.float64
 
-def mincover(graph: nx.Graph)->set:
+
+def mincover(graph: nx.Graph) -> set:
     """
     Return a minimum-cardinality vertex cover in the given graph.
     
@@ -18,25 +20,20 @@ def mincover(graph: nx.Graph)->set:
     >>> len(mincover(nx.Graph([])))
     0
     """
-    # Your code here
     var = {node: cvxpy.Variable(boolean=True) for node in graph.nodes}
-    objective = sum(var[node]
-        for node in graph.nodes
-    )   
-    constraints = [
-        var[u] + var[v] >= 1 for u,v in graph.edges
-    ]
+    objective = cvxpy.sum(var[node] for node in graph.nodes)
+    constraints = [var[u] + var[v] >= 1 for u, v in graph.edges]
     prob = cvxpy.Problem(cvxpy.Minimize(objective), constraints)
     prob.solve(solver=cvxpy.SCIPY)
-    return {node for node,nodevar in var.items() if nodevar.value>0}
+    return {node for node, nodevar in var.items() if nodevar.value > 0}
 
 
 if __name__ == '__main__':
     import doctest
+
     print(doctest.testmod())
 
     # Use this code for testing via console input-output:
     # edges=eval(input())
     # graph = nx.Graph(edges)
     # print(len(mincover(graph)))
-
--- mincover_test.py
+++ mincover_test.py
@@ -1,14 +1,19 @@
+from time import perf_counter
+
 import pytest
 import networkx as nx
 from mincover import mincover
 from testcases import parse_testcases
+import random
 
 testcases = parse_testcases("testcases.txt")
 
-def run_testcase(input:str):
+
+def run_testcase(input: str):
     graph = nx.Graph(input)
     cover = mincover(graph)
     return len(cover)
+
 
 @pytest.mark.parametrize("testcase", testcases, ids=[testcase["name"] for testcase in testcases])
 def test_cases(testcase):
@@ -16,6 +21,54 @@ def test_cases(testcase):
     assert actual_output == testcase["output"], f"Expected {testcase['output']}, got {actual_output}"
 
 
-def test_new_cases():
-    # your new tests here
-    pass
+def test_complete():
+    nodes = 100
+    graph = nx.complete_graph(nodes)
+    cover_kn = mincover(graph)
+    assert valid_cover(graph, cover_kn)
+    assert len(cover_kn) == nodes - 1
+
+
+def test_star():
+    nodes = 20
+    star = nx.star_graph(nodes - 1)
+    cover_star = mincover(star)
+    assert len(cover_star) == 1
+
+
+def test_no_edges():
+    nodes = 1000
+    graph = nx.gnm_random_graph(nodes, 0)
+    assert len(mincover(graph)) == 0
+
+
+def test_disconnected():
+    graph = nx.Graph()
+    graph.add_edges_from([(1, 2), (3, 4), (5, 6)])
+    cover_dis = mincover(graph)
+    assert valid_cover(graph, cover_dis)
+    assert len(cover_dis) == 3
+
+
+def test_random():
+    random.seed(42)
+    nodes, edges = 50, 1000
+    large_graph = nx.gnm_random_graph(nodes, edges)
+
+    assert measure_time(mincover, large_graph) < 1.0
+    assert valid_cover(large_graph, mincover(large_graph))
+
+
+def measure_time(func, *args, **kwargs):
+    start = perf_counter()
+    func(*args, **kwargs)
+    end = perf_counter()
+    return end - start
+
+
+def valid_cover(G, cover):
+    cover_set = set(cover)
+    for u, v in G.edges():
+        if u not in cover_set and v not in cover_set:
+            return False
+    return True
